1a45b4524d703e3cafb205794c9647f0cd10c989
Gavin <gavinchrisdsilva@gmail.com>
2018-09-21 19:09:59 +0530

    Added grid wrap around such that edge case do not need to be handled separately.

--- Game_of_Life/sketch.js
+++ Game_of_Life/sketch.js
@@ -11,19 +11,15 @@ function getNextGrid(grid){
 
   for(let i=0; i<cols; i++){
     for(let j=0; j<rows; j++){
-      if( i==0 || i==cols-1 || j==0 || j==rows-1){
-        next[i][j] = grid[i][j];
-      } else {
-        let liveneighbours = countLiveNeighbours(i, j, grid);
-        if(grid[i][j] == 0){
-          if(liveneighbours == 3) next[i][j] = 1;
-          else next[i][j] = 0; 
-        }
-        else if(liveneighbours < 2 || liveneighbours > 3){
-          next[i][j] = 0;
-        }
-        else next[i][j] = 1;
+      let liveneighbours = countLiveNeighbours(i, j, grid);
+      if(grid[i][j] == 0){
+        if(liveneighbours == 3) next[i][j] = 1;
+        else next[i][j] = 0; 
       }
+      else if(liveneighbours < 2 || liveneighbours > 3){
+        next[i][j] = 0;
+      }
+      else next[i][j] = 1;
     }
   }
   return next;
@@ -33,7 +29,9 @@ function countLiveNeighbours(i, j, grid){
   let sum = 0;
   for(let m=-1; m<=1; m++){
     for(let n=-1; n<=1; n++){
-      sum += grid[i+m][j+n];
+      let colshift = (m+i+10)%10;
+      let rowshift = (n+j+10)%10;
+      sum += grid[colshift][rowshift];
     }
   }
   return sum;
@@ -43,10 +41,10 @@ let grid;
 let newgrid;
 let cols;
 let rows;
-let resolution = 40;
+let resolution = 20;
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(800, 600);
   cols = width/resolution;
   rows = height/resolution;
   grid = make2DArray(cols, rows);
